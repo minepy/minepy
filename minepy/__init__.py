0d39f2c0f2f0f7b2e180a300f48af6a3a7a4ff10
Davide Albanese <davide@tesla.intra.ismaa.it>
2017-02-23 14:07:05 +0100

    update __init__.py

--- minepy/__init__.py
+++ minepy/__init__.py
@@ -1,4 +1,4 @@
-from mine import MINE, pstats, cstats
-from mine import version as __version__
+from .mine import MINE, pstats, cstats
+from .mine import version as __version__
 
 __all__ = ["MINE", "pstats", "cstats"]
